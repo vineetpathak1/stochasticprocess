7cfcc91f18ee5b372d4ced1799d9b7440b54552f
Vineet Pathak <vineetpathak1@gmail.com>
2021-09-22 10:12:08 +0530

    fundamental matrix

--- GamblersRuin.py
+++ GamblersRuin.py
@@ -5,7 +5,7 @@ from numpy import linalg  as la
 
 from scipy import linalg  as scla
 
-success_prob = 0.45
+success_prob = 0.4
 failure_prob = 1 - success_prob
 
 p = np.array([  [1, 0,0,0,0,0,0,0,0,0,0 ], 
@@ -26,7 +26,7 @@ initial_dist = np.array([[0,0,0,0,0,1,0,0,0,0,0]])
 print(np.round(p,15))
 
 
-n= 100
+n= 1000
 # for i in range(2) :
 p2 = np.round(matrix_power(p, n), 3)   
 # print (n, p2) 
